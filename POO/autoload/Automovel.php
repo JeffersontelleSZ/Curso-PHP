5addffa891fb9fae858ef9ba750d64f7f92c9671
Jefferson Teles <jefferson_dev@outlook.com>
2019-08-16 09:03:53 -0300

    Function autoload

---
+++ POO/autoload/Automovel.php
@@ -0,0 +1,25 @@
+<?php
+interface Veiculo
+{
+  public function acelerar($velocidade);
+  public function freiar($velocidade);
+  public function trocarMarcha($marcha);
+}
+
+abstract class Automovel implements Veiculo
+{
+  public function acelerar($velocidade)
+  {
+    echo 'A velocidade do seu veiculo é de ' . $velocidade . ' km/h';
+  }
+
+  public function freiar($velocidade)
+  {
+    echo 'O veiculo freio na velocidade ' . $velocidade . ' km/h';
+  }
+
+  public function trocarMarcha($marcha)
+  {
+    echo 'O veiculo trocou para a marcha ' . $marcha;
+  }
+}
